76ce8d481280117fba38a2364a53fe5a2650de4a
Peyton Mastropolo <amyhytwn@aol.com>
2024-05-15 22:46:46 -0500

    Update Contactpage.js

--- components/Contactpage/Contactpage.js
+++ components/Contactpage/Contactpage.js
@@ -63,11 +63,11 @@ const Contactpage = () => {
                     </div>                
                 </div>
             </div> 
-            <section className="wpo-contact-map-section">
-                <div className="wpo-contact-map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.9147703055!2d-74.11976314309273!3d40.69740344223377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbd!4v1547528325671"></iframe>
-                </div>
-            </section>
+ <section className="wpo-contact-map-section">
+    <div className="wpo-contact-map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3328.2983179949936!2d-86.81617908502029!3d33.24432870664067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8889230017b23e83%3A0x1f62791f94c85638!2sAlabaster%2C%20AL%2C%20USA!5e0!3m2!1sen!2sbd!4v1547528325671"></iframe>
+    </div>
+</section>
         </section>
      )
         
